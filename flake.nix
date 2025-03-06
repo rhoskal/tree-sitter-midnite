29e483a2d60df3445db821900f42c9a3117507b6
Hans Hoffman <github.backrest158@passmail.net>
2025-03-06 16:56:44 -0700

    setup nix flake

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "aarch64-darwin";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs =
+          with pkgs;
+          [
+            nixd
+            nixfmt-rfc-style
+            nodejs_23
+            pnpm_10
+          ]
+          ++ (
+            if stdenv.isLinux then
+              [
+                inotify-tools
+                libnotify
+              ]
+            else if stdenv.isDarwin then
+              [
+                terminal-notifier
+                darwin.apple_sdk.frameworks.CoreFoundation
+                darwin.apple_sdk.frameworks.CoreServices
+              ]
+            else
+              [ ]
+          );
+
+        shellHook = ''
+          export LC_ALL=en_US.UTF-8
+        '';
+      };
+    };
+}
