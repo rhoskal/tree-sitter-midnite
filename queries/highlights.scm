a47639bfea7c73e85af2040a7f81267414447c31
Hans Hoffman <github.backrest158@passmail.net>
2025-03-07 12:00:31 -0700

    add more queries

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,6 +1,5 @@
-(comment) @comment
-((comment) @doc_comment
-  (#match? @doc_comment "^##"))
+(line_comment) @comment
+(doc_comment) @comment.doc
 
 (identifier) @variable
 (type_annotation) @type
@@ -8,3 +7,61 @@
 (function_definition name: (identifier) @function)
 (module_declaration name: (identifier) @module)
 (match_expression) @conditional
+
+; Delimiter
+
+"(" @punctuation.bracket
+")" @punctuation.bracket
+"[" @punctuation.bracket
+"]" @punctuation.bracket
+"{" @punctuation.bracket
+"}" @punctuation.bracket
+
+":" @punctuation.delimiter
+"." @punctuation.delimiter
+"," @punctuation.delimiter
+
+; Keyword
+
+"alias" @keyword
+"as" @keyword
+"else" @keyword
+"end" @keyword
+"exposing" @keyword
+"fn" @keyword
+"foreign" @keyword
+"hiding" @keyword
+"include" @keyword
+"infixl" @keyword
+"infixn" @keyword
+"infixr" @keyword
+"let" @keyword
+"match" @keyword
+"module" @keyword
+"on" @keyword
+"open" @keyword
+"then" @keyword
+"type" @keyword
+"using" @keyword
+"when" @keyword
+
+; Operator
+
+"+." @operator
+"-." @operator
+"/." @operator
+"*." @operator
+"+" @operator
+"-" @operator
+"/" @operator
+"*" @operator
+"::" @operator
+"<>" @operator
+"==" @operator
+"/=" @operator
+"<" @operator
+"<=" @operator
+">" @operator
+">=" @operator
+"&&" @operator
+"||" @operator
