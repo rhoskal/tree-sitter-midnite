f029b752f2871c890d608ff3b430c95885686a73
Hans Hoffman <github.backrest158@passmail.net>
2025-03-10 14:16:01 -0600

    add spacing

--- queries/indents.scm
+++ queries/indents.scm
@@ -16,18 +16,23 @@
 ;; Pattern matching
 
 (match_case) @indent
+
 (guard_condition) @indent
 
 ;; Function bodies
 
 (lambda_expression "=>" @indent)
+
 (match_case "=>" @indent)
 
 ;; Type definitions
 
 (type_declaration "=" @indent)
+
 (type_alias_declaration "=" @indent)
+
 (function_declaration "=" @indent)
+
 (foreign_function_declaration "=" @indent)
 
 ;; Brackets for indentation
@@ -50,10 +55,15 @@
 ;; Align these with their opening counterparts
 
 (parameter_list) @align
+
 (tuple_expression) @align
+
 (list_expression) @align
+
 (record_type) @align
+
 (exposing_list) @align
+
 (type_parameters) @align
 
 ;; Doc comments
